bac16d63db4ff49e778fe59cc2cf7ced4b334710
ujihisa <ujihisa@gmail.com>
2010-12-24 15:59:21 -0800

    implementing run() without considering with()

--- lib/srap.rb
+++ lib/srap.rb
@@ -13,12 +13,26 @@ class Srap
     @code = code
   end
 
-  def with(initials)
-    p initials
-    run
+  def with(initials, &block)
+    @initials = initials
+    run(&block)
   end
 
-  def run
-    p :cool
+  def run(&block)
+    s = StringScanner.new(@code)
+    memo = []
+    until s.eos?
+      memo << block.call(s)
+    end
+    memo.inject([]) {|m, token|
+      if /\!$/ =~ token[0].to_s && m[-1] && m[-1][0] == token[0]
+        m[-1][1] << token[1]
+        m
+      else
+        m.push token
+      end
+    }.map {|name, value|
+      [name.to_s.sub(/\!$/, '').to_sym, value]
+    }
   end
 end
--- spec/srap_spec.rb
+++ spec/srap_spec.rb
@@ -1,12 +1,20 @@
 require_relative '../lib/srap'
 describe 'Srap()' do
   it 'is awesome' do
-    Srap(<<-EOH).with(in_comment: false)
+    code = <<-EOH
     asjkdl
     jasdfkl
     # asdjkl
     iadsjfk #fjdaskl
     asdjfkl
     EOH
+    Srap(code).with(in_comment: false) {|s|
+      case
+      when s.scan(/#.*/)
+        [:comment, s[0]]
+      when s.scan(/./m)
+        [:normal!, s[0]]
+      end
+    }.should == [[:normal, "    asjkdl\n    jasdfkl\n    "], [:comment, "# asdjkl"], [:normal, "\n    iadsjfk "], [:comment, "#fjdaskl"], [:normal, "\n    asdjfkl\n"]]
   end
 end
